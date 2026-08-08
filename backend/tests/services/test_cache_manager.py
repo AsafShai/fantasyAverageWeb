7d19dc6ad08a856ac98b687364e526ddcc7ae631
Asaf Shai <asafshai211@gmail.com>
2026-08-08 17:42:32 +0300

    chore(backend): remove dead CacheManager.get_totals/get_players wrappers

--- backend/tests/services/test_cache_manager.py
+++ backend/tests/services/test_cache_manager.py
@@ -13,47 +13,10 @@ def reset_cache_manager():
 
 
 class TestCacheManager:
-    def test_miss_then_hit_totals(self):
-        cm = CacheManager()
-        calls = {"n": 0}
-
-        def calc():
-            calls["n"] += 1
-            return pd.DataFrame({"a": [1]})
-
-        df1 = cm.get_totals("etag-a", calc)
-        df2 = cm.get_totals("etag-a", calc)
-        assert calls["n"] == 1
-        pd.testing.assert_frame_equal(df1, df2)
-
-    def test_new_etag_recomputes(self):
-        cm = CacheManager()
-        calls = {"n": 0}
-
-        def calc():
-            calls["n"] += 1
-            return pd.DataFrame({"x": [calls["n"]]})
-
-        cm.get_totals("e1", calc)
-        cm.get_totals("e2", calc)
-        assert calls["n"] == 2
-
-    def test_calculator_returns_none_does_not_cache(self):
-        cm = CacheManager()
-        calls = {"n": 0}
-
-        def calc():
-            calls["n"] += 1
-            return None
-
-        assert cm.get_totals("e", calc) is None
-        assert cm.get_totals("e", calc) is None
-        assert calls["n"] == 2
-
     def test_invalidate_clears_both(self):
         cm = CacheManager()
-        cm.get_totals("t", lambda: pd.DataFrame({"a": [1]}))
-        cm.get_players("p", lambda: pd.DataFrame({"b": [2]}))
+        cm.totals_cache = {"etag": "t", "data": pd.DataFrame({"a": [1]})}
+        cm.players_cache = {"etag": "p", "data": pd.DataFrame({"b": [2]})}
         info = cm.get_cache_info()
         assert info["has_totals"] and info["has_players"]
         cm.invalidate_cache()
